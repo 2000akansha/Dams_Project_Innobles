8bc4a40115281850bf371534428e7905946a0fb5
camelCase <153457928+2000akansha@users.noreply.github.com>
2024-09-30 11:35:54 +0530

    Update mobileUserRoute.js

--- src/mobileApi/mobileRoutes/mobileUserRoute.js
+++ src/mobileApi/mobileRoutes/mobileUserRoute.js
@@ -1,21 +1,19 @@
 import express from 'express';
 import { register, login, logout } from '../mobileController/userController.js';
-import { getVillageDetails, getVillageDetailsWithBeneficiaryCount, uploadDocs } from '../mobileController/dashboardController.js';
-import { isAuthenticated } from '../../middleware/auth.js';
+import { getVillageDetails, getBeneficiariesByKhatauniSankhya, uploadDocs } from '../mobileController/dashboardController.js';
 import uploadDocsMiddleware from '../../middleware/multerconfig.js'; // Import the middleware
-
 const router = express.Router();
-
 // User routes
 router.post('/register', register);
 router.post('/login', login);
 router.get('/logout', logout);
-
 // Village details routes
-router.get('/village/', isAuthenticated, getVillageDetails);
-router.get('/villages/details-with-count', isAuthenticated, getVillageDetailsWithBeneficiaryCount);
+router.get('/village/', getVillageDetails);
+router.get('/villages/details-with-count', getBeneficiariesByKhatauniSankhya);
 
-// Document upload route
-router.post('/uploadDocs', isAuthenticated, uploadDocsMiddleware, uploadDocs);
+
+const numberOfBeneficiaries = 50; // Adjust as needed
+
+router.post('/upload-docs', uploadDocsMiddleware(numberOfBeneficiaries), uploadDocs);
 
 export default router;
